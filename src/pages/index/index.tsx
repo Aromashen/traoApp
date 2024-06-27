c825d611ee97ed331e8c6fdb502ebc18017eb9db
shenfei <shenfei@vcredit.com>
2024-06-27 18:13:17 +0800

    新增首页

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,6 +1,7 @@
-import { View, Text } from '@tarojs/components'
+import { View, Text, Image } from '@tarojs/components'
 import { useLoad } from '@tarojs/taro'
 import './index.scss'
+import more from "../../assets/more.png"
 
 export default function Index() {
 
@@ -10,7 +11,10 @@ export default function Index() {
 
   return (
     <View className='index'>
-      <Text>Hello world!</Text>
+     <View className="header">
+      <Text>首页</Text>
+      <Image src={more} style={{width:'12px',height:'12px'}} className="icon"></Image>
+     </View>
     </View>
   )
 }

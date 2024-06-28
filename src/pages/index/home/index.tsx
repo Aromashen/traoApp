d912e927e6ce699089cf1d1ec01db1f26aeb0715
shenfei <shenfei@vcredit.com>
2024-06-28 16:57:28 +0800

    下拉

--- src/pages/index/home/index.tsx
+++ src/pages/index/home/index.tsx
@@ -1,6 +1,5 @@
 import { View, Text } from '@tarojs/components'
 import { useLoad } from '@tarojs/taro'
-import './index.scss'
 
 export default function Index() {
 
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,6 @@
 import { View, Text, Image } from '@tarojs/components'
 import { useLoad } from '@tarojs/taro'
+import { useState } from 'react'
 import './index.scss'
 import more from "../../assets/more.png"
 
@@ -8,13 +9,23 @@ export default function Index() {
   useLoad(() => {
     console.log('Page loaded.')
   })
+  const [toggle,setToggle] = useState(false)
+  const menus = [{name:'首页',value:'home'},{name:'关于',value:'about'}]
+  const toggleMenu = ()=>{
+    setToggle(pre=>!pre)
+  }
 
   return (
     <View className='index'>
-     <View className="header">
+     {!toggle&&<View className="header">
       <Text>首页</Text>
-      <Image src={more} style={{width:'12px',height:'12px'}} className="icon"></Image>
-     </View>
+      <Image src={more} style={{width:'12px',height:'12px'}} className="icon" onClick={toggleMenu}></Image>
+     </View>}
+     {toggle&&<View className='dropDown'>
+        {menus.map(item=>(<View>
+          <Text>{item.name}</Text>
+        </View>))}
+        </View>}
     </View>
   )
 }

3a251e9e829a3c9e9c95f343dfc331cc733634b6
shenfei <shenfei@vcredit.com>
2024-08-16 15:23:17 +0800

    提交一版

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -2,58 +2,58 @@ import { View, Text, Image,ScrollView } from '@tarojs/components'
 import { useLoad,useReachBottom } from '@tarojs/taro'
 import Taro from '@tarojs/taro'
 
-import { useState,useEffect } from 'react'
+import { useState,useEffect,useRef } from 'react'
 import './index.scss'
 import more from "../../assets/more.png"
 import cross from "../../assets/cross.png"
 import logo from "../../assets/logo.webp"
 
 export default function Index() {
+  const right = useRef(null)
   const mockData = [
     {
     title:'旅游',
     value:'tour',
+    isSelected:false,
     subData:[
       {
       title:'一日游',
       value:"daliyTour",
+      isSelected:false,
     },
     {
       
       title:'跟团游',
       value:"followTour",
-
+      isSelected:false,
     },
     {
       
       title:'自由行',
       value:"freeTour",
-
-    
+      isSelected:false,
     }
-    
       ] 
     },
     {
       title:'酒店',
       value:'hotel',
+      isSelected:false,
       subData:[
         {
         title:'快捷酒店',
         value:"quickHotel",
+        isSelected:false,
       },
       {
-        
         title:'跟团游',
         value:"followTour",
-
+        isSelected:false,
       },
-      {
-        
+      { 
         title:'自由行',
         value:"freeTour",
-
-      
+        isSelected:false,
       }
       
         ] 
@@ -86,11 +86,19 @@ export default function Index() {
 
   }
   const clickLeft = (item)=>{
+    console.log('title',item.title)
+
     mockData.forEach(left=>{
       if(item.title==left.title){
         item.isSelected = !item.isSelected
       }
     })
+  }
+  const scrollRight = ()=>{
+    console.log('scroll',right.current)
+  }
+  const test = (subItem)=>{
+    console.log('subItem',subItem)
   }
 
   return (
@@ -114,8 +122,19 @@ export default function Index() {
      </>}
      <View className='container'>
       <ScrollView className='left'>
-        {mockData.map(item=>(<View key={item.value} className="leftItem" onClick={()=>clickLeft(item)}>
+        {mockData.map(item=>(<View key={item.value} onClick={()=>clickLeft(item)} className={["leftItem",item.isSelected?'active':'normal'].join(' ')}>
           <Text>{item.title}</Text>
+        </View>))}
+      </ScrollView>
+      <ScrollView className="right" onScroll={scrollRight} ref={right}>
+        {mockData.map(item=>(<View>
+          <Text className={item.isSelected?'activeTitle':'normalTitle'}>{item.title}</Text>
+          <View className="subContainer">
+            {item.subData.map(subItem=>(<View className="sub" onClick={()=>test(subItem)}>
+              <Text>{subItem.title}</Text>
+              
+            </View>))}
+          </View>
         </View>))}
       </ScrollView>
 
